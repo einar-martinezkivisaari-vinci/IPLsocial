6d92ea5da2ce882c05472ba5d6ec6773ea1674b1
Einar Martinez Kivisaari <einar.martinez@student.vinci.be>
2026-01-16 16:38:05 +0100

    refactored validmail to regroup its guards in a single line

--- src/validmail.ts
+++ src/validmail.ts
@@ -1,7 +1,4 @@
 export const validmail = (email:string) => {
-    if(!email.includes("@") || email.includes(" ") || email.endsWith("."))return false;
-    const dom = email.substring(email.lastIndexOf("@"));
-    if(!dom.includes("."))return false;
-    if(email.substring(0, email.lastIndexOf("@")).length === 0)return false;
+    if(!email.includes("@") || email.includes(" ") || email.endsWith(".") || !email.substring(email.lastIndexOf("@")).includes(".") || email.substring(0, email.lastIndexOf("@")).length === 0)return false;
     return true;
 }
